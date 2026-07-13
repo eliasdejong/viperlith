a5a80707b44e8576ee959ae6c674d915e59331ac
Elias de Jong <elias@fastserial.com>
2026-07-14 01:07:57 +0200

    Use multiple workers in prod launch script

--- prod_launch.sh
+++ prod_launch.sh
@@ -4,6 +4,7 @@ trap 'kill -KILL -$$' EXIT
 set -a; source .env; set +a
 
 export DEBUG="0"
+export WEB_CONCURRENCY=$(($(nproc) - 2 > 1 ? $(nproc) - 2 : 1))
 
 uv run python src/writer.py &
 uv run uvicorn src.main:app --loop uvloop --host 0.0.0.0 --port 8000 &
