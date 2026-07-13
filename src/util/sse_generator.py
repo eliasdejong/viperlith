527c5e4848cae9eff796f039a92fe9be69eb0af4
Elias de Jong <elias@fastserial.com>
2026-07-14 01:08:03 +0200

    Use double quote string

--- src/util/sse_generator.py
+++ src/util/sse_generator.py
@@ -29,4 +29,4 @@ async def sse_generator(render: Callable[..., str], *args, **kwargs) -> AsyncGen
 			)
 	except Exception as e:
 	# except asyncio.CancelledError as e:
-		print(f'Stream ended: {type(e).__name__}: {e}', flush=True)
+		print(f"Stream ended: {type(e).__name__}: {e}", flush=True)
