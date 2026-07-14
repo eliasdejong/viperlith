e92a01d7b25e2efc72880cb552ca149e981ff90f
Elias de Jong <elias@fastserial.com>
2026-07-14 16:23:19 +0200

    Cleanup shared memory before startup

--- debug_launch.sh
+++ debug_launch.sh
@@ -4,6 +4,9 @@ trap 'kill -KILL -$$' EXIT
 set -a; source .env; set +a
 
 export DEBUG="1"
+export WEB_CONCURRENCY="1"
+
+rm -f /dev/shm/datastar-gpt-cmdqueue-*
 
 uv run watchfiles "python src/writer.py" . &
 uv run uvicorn src.main:app --loop uvloop --reload --log-level debug &
--- prod_launch.sh
+++ prod_launch.sh
@@ -6,6 +6,8 @@ set -a; source .env; set +a
 export DEBUG="0"
 export WEB_CONCURRENCY=$(($(nproc) - 2 > 1 ? $(nproc) - 2 : 1))
 
+rm -f /dev/shm/datastar-gpt-cmdqueue-*
+
 uv run python src/writer.py &
 uv run uvicorn src.main:app --loop uvloop --host 0.0.0.0 --port 8000 &
 
