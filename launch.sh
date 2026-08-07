65b91cf717be0c6a434255f86ecabc50a0816d69
Elias de Jong <elias@fastserial.com>
2026-08-07 17:15:32 +0200

    Update launch script

--- launch.sh
+++ launch.sh
@@ -5,10 +5,4 @@ set -a; source .env; set +a
 export DEBUG="${DEBUG:-1}"
 export PYTHONPATH=.
 
-# if [ "$DEBUG" -eq 1 ]; then
-# 	uv run uvicorn src.main:app --loop uvloop --reload --log-level debug
-# else
-# 	uv run uvicorn src.main:app --loop uvloop --host 0.0.0.0 --port 8000
-# fi
-
 uv run src/main.py
