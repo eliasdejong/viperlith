81daae1612edd8658cfac0620cec31c6277e7460
Elias de Jong <elias@fastserial.com>
2026-08-24 22:14:52 +0200

    Benchmark 1k connections

--- benchmarks/run-S-R95.sh
+++ benchmarks/run-S-R95.sh
@@ -4,5 +4,5 @@ set -a; source .env.bench-small; set +a
 
 export WRK_READ_PCT=95
 
-wrk -t4 -c64 -d60s -s common.lua --latency http://127.0.0.1:8000
+wrk -t4 -c1k -d60s -s common.lua --latency http://127.0.0.1:8000
 
