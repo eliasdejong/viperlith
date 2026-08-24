253d41d0d62e05f43359d9eff6c245ae8ef5cdd9
Elias de Jong <elias@fastserial.com>
2026-08-24 08:33:18 +0200

    Make migrations dir relative

--- src/util/db_migration.py
+++ src/util/db_migration.py
@@ -2,12 +2,18 @@ import os
 from src.util.db_write_con import con
 
 
+MIGRATIONS_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../migrations")
+
+
 def run_migration():
-	for fname in sorted(os.listdir("migrations")):
+	for fname in sorted(os.listdir(MIGRATIONS_DIR)):
 		if not fname.endswith(".sql"):
 			continue
 		migr_version = int(fname.split("_")[0])
 		if migr_version > con.pragma("user_version"):
-			with open(os.path.join("migrations", fname)) as f:
+			with open(os.path.join(MIGRATIONS_DIR, fname)) as f:
 				con.execute(f.read())
 			con.pragma("user_version", migr_version)
+
+if __name__ == "__main__":
+	run_migration()
