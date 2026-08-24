e0012b286ed0c5dfd3322a364ee66118acefb302
Elias de Jong <elias@fastserial.com>
2026-08-24 08:41:13 +0200

    Add simulated session ids

--- src/util/session_id.py
+++ src/util/session_id.py
@@ -1,11 +1,17 @@
+import os, random
 from uuid import UUID, uuid4
 from litestar import Request
 
 
-def get_session_id(request: Request) -> str:
-	try:
-		return str(UUID(request.session.get("id")))
-	except:
-		sid = str(uuid4())
-		request.session["id"] = sid
-		return sid
+if "SIMULATED_SESSION_COUNT_MAX" in os.environ:
+	SIMULATED_SESSION_COUNT_MAX = int(os.getenv("SIMULATED_SESSION_COUNT_MAX"))
+	def get_session_id() -> str:
+		return f"s_{random.randint(1, SIMULATED_SESSION_COUNT_MAX):08d}"
+else:
+	def get_session_id(request: Request) -> str:
+		try:
+			return str(UUID(request.session.get("id")))
+		except:
+			sid = str(uuid4())
+			request.session["id"] = sid
+			return sid
