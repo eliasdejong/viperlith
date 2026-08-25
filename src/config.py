a10a62c323135bc55170305385b813dcb0579a7d
Elias de Jong <elias@fastserial.com>
2026-08-25 11:37:25 +0200

    Increase `msg_send` ring buffer

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ import os
 # identifier : size (bytes)
 QUEUE_SIZES = {
 	"user_insert": 4096,
-	"msg_send": 8192,
+	"msg_send": 32768,
 	"channel_open": 4096,
 	"channel_close": 4096,
 	"nickname_set": 4096,
