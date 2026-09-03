de7dd438cd2d8e76cb6792c89fe2c2c7e5ecd3e6
Elias de Jong <elias@fastserial.com>
2026-09-03 11:52:26 +0200

    Set synchronous to normal

--- src/util/db_write_con.py
+++ src/util/db_write_con.py
@@ -15,7 +15,7 @@ con.pragma("page_size", 4096)
 con.pragma("journal_mode", "wal")
 con.pragma("journal_size_limit", 33554432) # 32 MiB; prevent WAL from growing unbounded
 con.pragma("wal_autocheckpoint", -1)
-con.pragma("synchronous", "off")
+con.pragma("synchronous", "normal")
 
 con.pragma("cache_size", -8192)
 con.pragma("busy_timeout", 5000)
