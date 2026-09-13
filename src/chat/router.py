bcfc987bae6e9b43231ff986fae5fe93a699960f
Elias de Jong <elias@fastserial.com>
2026-09-13 17:01:30 +0200

    Move off deprecated implicit dependency

--- src/chat/router.py
+++ src/chat/router.py
@@ -49,19 +49,19 @@ async def get_updates(request: Request, sid: NamedDependency[str]) -> Stream:
 	)
 
 @post("/chat/message-send", sync_to_thread=False, status_code=204)
-def post_msg_send(request: Request, sid: NamedDependency[str], signals_json: Any) -> None:
+def post_msg_send(request: Request, sid: NamedDependency[str], signals_json: NamedDependency[Any]) -> None:
 	model = msg_send_decoder.decode(signals_json)
 	model.session_id = sid
 	srt.put(Q.msg_send, msgpack_encoder.encode(model))
 
 @post("/chat/channel-open", sync_to_thread=False, status_code=204)
-def post_channel_open(request: Request, sid: NamedDependency[str], signals_json: Any) -> None:
+def post_channel_open(request: Request, sid: NamedDependency[str], signals_json: NamedDependency[Any]) -> None:
 	model = channel_open_decoder.decode(signals_json)
 	model.session_id = sid
 	srt.put(Q.channel_open, msgpack_encoder.encode(model))
 
 @post("/chat/channel-open/validate", sync_to_thread=False, status_code=200)
-def post_channel_open_validate(request: Request, signals_json: Any) -> dict[str, str]:
+def post_channel_open_validate(request: Request, signals_json: NamedDependency[Any]) -> dict[str, str]:
 	try:
 		channel_open_decoder.decode(signals_json)
 		return {"_channelOpenError": ""}
@@ -69,13 +69,13 @@ def post_channel_open_validate(request: Request, signals_json: Any) -> dict[str,
 		return {"_channelOpenError": "Invalid channel name"}
 
 @post("/chat/channel-close", sync_to_thread=False, status_code=204)
-def post_channel_close(request: Request, sid: NamedDependency[str], signals_json: Any) -> None:
+def post_channel_close(request: Request, sid: NamedDependency[str], signals_json: NamedDependency[Any]) -> None:
 	model = channel_close_decoder.decode(signals_json)
 	model.session_id = sid
 	srt.put(Q.channel_close, msgpack_encoder.encode(model))
 
 @post("/chat/nickname-set", sync_to_thread=False, status_code=200)
-def post_nickname_set(request: Request, sid: NamedDependency[str], signals_json: Any) -> dict[str, str]:
+def post_nickname_set(request: Request, sid: NamedDependency[str], signals_json: NamedDependency[Any]) -> dict[str, str]:
 	try:
 		model = nickname_set_decoder.decode(signals_json)
 		model.session_id = sid
