ba54a3573f0a4ae39588c2232bf13ed753b68453
Elias de Jong <elias@fastserial.com>
2026-09-15 20:59:45 +0200

    Fix SQL

--- src/single_writer.py
+++ src/single_writer.py
@@ -37,7 +37,7 @@ def writer_tick():
 			from users u
 			where u.session_id = :session_id;
 
-			update users u
+			update users as u
 			set current_channel_id = (select id from channels where name = :channelOpen)
 			where u.session_id = :session_id;
 			""",
